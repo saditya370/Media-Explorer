4a2042993726138024f765b64578a2ae200e0425
= <=>
2022-12-27 10:54:38 +0530

    bug fixed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,12 +8,10 @@ class PhotosController < ApplicationController
     all_photos = photo_params[:image]
     all_photos.shift
     pos1 =0
-    all_photos.each do |photo|
-      if @album.photos.count >= 1
-        pos1 = @album.photos.last.position if @album.photos.last.position != nil 
-      end
-      @photo = @album.photos.create!(image: photo,position: pos1 += 1)
-   
+    all_photos.each do |photo|  
+      pos1 = @album.photos.count  if @album.photos.count >= 1
+      @photo = @album.photos.create!(image: photo,position: pos1 += 1 )
+      
     end
 
     if @photo.save
